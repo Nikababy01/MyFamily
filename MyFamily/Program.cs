52cb214de205cf7a36432748221aa61d172e5966
Monique.Bass01 <monique.bass01@gmail.com>
2020-08-20 17:52:28 -0500

    Add project files.

---
+++ MyFamily/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyFamily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var myFamily = new Dictionary<string, Dictionary<string, string>>();
+            myFamily.Add("sister", new Dictionary<string, string>() { { "name", "Krista" }, { "age", "42" } });
+            myFamily.Add("husband", new Dictionary<string, string>() { { "name", "Ray" }, { "age", "46" } });
+            myFamily.Add("son", new Dictionary<string, string>() { { "name", "Ray Jr" }, { "age", "10" } });
+            myFamily.Add("daughter", new Dictionary<string, string>() { { "name", "Chasity" }, { "age", "27" } });
+            myFamily.Add("Mother", new Dictionary<string, string>() { { "name", "Marian" }, { "age", "65" } });
+            myFamily.Add("brother", new Dictionary<string, string>() { { "name", "Dedrick" }, { "age", "35" } });
+            myFamily.Add("grandchild", new Dictionary<string, string>() { { "name", "Chase" }, { "age", "8" } });
+            foreach (var person in myFamily)
+            {
+                Console.WriteLine($"I love my {person.Key} {person.Value["name"]} and they are {person.Value["age"]} years old.");
+            }
+
+        }
+    }
+}
